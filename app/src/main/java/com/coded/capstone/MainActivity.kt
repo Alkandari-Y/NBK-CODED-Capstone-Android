bec7d43c78a195885d6c7bb066215047b183cb2f
Bashayer770 <Beshow95@gmail.com>
2025-06-17 13:07:42 +0300

    small bracket fix

--- app/src/main/java/com/coded/capstone/MainActivity.kt
+++ app/src/main/java/com/coded/capstone/MainActivity.kt
@@ -5,22 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.coded.capstone.MapAndGeofencing.LocationPermissionHandler
 import com.coded.capstone.ui.theme.CapstoneTheme
-
 import com.coded.capstone.navigation.AppHost
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,27 +21,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-
             CapstoneTheme {
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    LocationPermissionHandler(
-//                        onPermissionGranted = {
-//                            // Permissions granted, geofence service will be started by LocationPermissionHandler
-//                        }
-//                    )
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-
-            MaterialTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    AppHost()
-
+                    MaterialTheme {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+//                            LocationPermissionHandler(
+//                                onPermissionGranted = {
+//                                    // Permissions granted, geofence service will be started by LocationPermissionHandler
+//                                }
+//                            )
+                            AppHost()
+                        }
+                    }
                 }
             }
         }
