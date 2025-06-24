b4bfa1e70d38b5b51905ffbfc3a14f470bf13847
fatemahAlMomen <fatemah.alm@hotmail.com>
2025-06-24 20:33:33 +0300

    Integrated Promotions to Calender

--- app/src/main/java/com/coded/capstone/data/responses/promotion/PromotionResponse.kt
+++ app/src/main/java/com/coded/capstone/data/responses/promotion/PromotionResponse.kt
@@ -1,5 +1,6 @@
 package com.coded.capstone.data.responses.promotion
 
+import com.google.gson.annotations.SerializedName
 import java.time.LocalDate
 
 data class PromotionResponse(
@@ -7,11 +8,19 @@ data class PromotionResponse(
     val name: String,
     val businessPartnerId: Long,
     val type: RewardType,
-    val startDate: LocalDate,
-    val endDate: LocalDate,
+    @SerializedName("startDate")
+    private val _startDate: String,
+    @SerializedName("endDate")
+    private val _endDate: String,
     val description: String,
     val storeId: Long?
-)
+) {
+    val startDate: LocalDate
+        get() = LocalDate.parse(_startDate)
+    
+    val endDate: LocalDate
+        get() = LocalDate.parse(_endDate)
+}
 
 enum class RewardType {
     CASHBACK, DISCOUNT
